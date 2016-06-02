14df4f5f02ab57d9882b9e7763401e208133a5a5
zhaolong-benhu <benhu_zl@163.com>
2016-06-02 16:49:20 +0800

    add some files

---
+++ js/src/index.js
@@ -0,0 +1,23 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import catch_render_error from './module/catch_error';
+
+import Xianzhi from './module/Xianzhi.js';
+
+@catch_render_error
+class MainPage extends React.Component{
+
+    render(){
+      return(
+        <div>
+          <Xianzhi />
+        </div>
+      )
+    }
+
+}
+
+ReactDOM.render(
+  <MainPage />,document.getElementById('app')
+);
